8e67dffa9950e611b5381883999ee06c95a91f70
Setephano <setephano@gmail.com>
2024-03-07 16:56:29 +1300

    add(ui): add tailwindcss with svelte - https://tailwindcss.com/docs/guides/sveltekit

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,8 +1,11 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+// import adapter from "@sveltejs/adapter-auto";
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
+  // kit: {
+  //   adapter: adapter()
+  // },
   preprocess: vitePreprocess(),
-}
-
+};
